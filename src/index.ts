509e33606726b99983a4d0ebfd7624652fbe8f8b
sah_314 <khadayatesahil@gmail.com>
2023-08-26 16:27:54 +0530

    Added middleware and controllers directory

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
 import express, { Request, Response } from 'express';
-import body_parser from "body-parser";
-import sql from "sqlite3";
+import bodyParser from 'body-parser';
+import routes from './routes';
+
 const app = express();
 
-const db = new sql.Database("database.db");
-app.use(body_parser.json())
-app.use(body_parser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req:Request,res:Response)=>{
-res.status(200).json({"message":"Success"});
-
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Success' });
 });
-app.post("/addTodo",(req:Request,res:Response)=>{
-    let todo:Object = {name:req.body.name,
-        description:req.body.description,
-        status:req.body.status};
-        console.log(todo);
-        res.json(todo);
-    }
 
-)
+app.use('/', routes);
 
-app.listen(3000,()=>{console.log("Listening on port 3000")});
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
