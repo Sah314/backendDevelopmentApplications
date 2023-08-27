e69def4b2ead27248c2ead6bd5896ac3866184eb
sah_314 <khadayatesahil@gmail.com>
2023-08-27 11:38:20 +0530

    Added jwt authentication to login method

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,36 @@
 import { Request,Response } from "express";
 import Users from "../Models/users";
 import bcrypt from "bcrypt";
-export const userLogin = (req:Request,res:Response)=>{
+import jwt from "jsonwebtoken";
+import path from 'path';
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+export const userLogin = async(req:Request,res:Response)=>{
  var {email,password} = req.body;
+ try {
+    if(!email ||!password){
+        return res.status(401).json({message:"please enter all the fields"});
+    }
+    const currUser= await Users.findOne({where:{email:email}});
+    if(currUser){
+        const pass:string= currUser.password;
+    const resu = await bcrypt.compare(password,pass);
+    if(resu){
+        let token = jwt.sign({id:currUser.id,email:currUser.email},process.env.SECRET_KEY as string) ;
+        return res.status(201).json({token:token,user:currUser});
+    }
+    else{
+        return res.status(401).json({message:"Incorrect password or email"});
+    }
+    }
+    return res.status(404).json({"message":"User not found!"});
+
+    
+ } catch (error) {
+    console.error(error);
+    return res.status(501).json({error:error});
+ }
+
 
 }
 export const userSignup = async(req:Request,res:Response)=>{
@@ -23,7 +51,8 @@ export const userSignup = async(req:Request,res:Response)=>{
         else{
             let newUser = await Users.create({name:username,email:email,password:hash});
             console.log("Users username is :", newUser.name);
-            return res.status(201).json({"message": "Successfully added user!"});
+            res.status(201).json({"message": "Successfully added user!"});
+            return res.redirect("../login");
         }
     });
         
