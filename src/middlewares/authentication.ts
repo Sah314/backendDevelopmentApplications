28c9156455cdc9427d3bfeba4fb00a5866907b20
sah_314 <khadayatesahil@gmail.com>
2023-08-27 11:37:42 +0530

    Created middleware for authentication

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,25 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request,Response,NextFunction } from 'express';
+
+export interface CustomRequest extends Request {
+    token: string | JwtPayload|undefined;
+   }
+
+const authenticateToken = (req:Request, res:Response, next:NextFunction)=> {
+  const token = req.headers.authorization?.split(' ')[1]; // Get the token from the Authorization header
+
+  if (token == null) {
+    return res.sendStatus(401); // Unauthorized
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY as string, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // Forbidden
+    }
+    (req as CustomRequest).token = user; // Attach the user data to the request object
+});
+next();
+}
+
+export default authenticateToken;
+
